aafedbc0b301993d18e93196859647b9aea401f1
Jaible <jaiblemd@gmail.com>
2022-06-15 13:23:20 -0600

    Merge origin/master
    Conflicts:
    	src/main/resources/static/index.html

---
+++ src/main/java/com/tienda/controller/PersonaController.java
@@ -0,0 +1,24 @@
+package com.tienda.controller;
+
+import com.tienda.entity.Persona;
+import com.tienda.service.IPersonaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PersonaController {
+    
+    @Autowired
+    private IPersonaService personaService;
+    
+    @GetMapping("/persona")
+    public String index (Model model){
+        List<Persona> listaPersona = personaService.getAllPersona();
+        model.addAttribute("titulo","Tabla personas");
+        model.addAttribute("personas",listaPersona);
+        return "personas";
+    }
+}
