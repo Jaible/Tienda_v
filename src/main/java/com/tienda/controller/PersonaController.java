cb9cb4565e12abc9c6c52b5f718041b2b7b6d4bb
Jaible <jaiblemd@gmail.com>
2022-07-06 20:28:56 -0600

    Merge origin/master
    Conflicts:
    	src/main/resources/static/Index.html
    	src/main/resources/static/UserProfile.html
    	src/main/resources/static/css/Style.css

--- src/main/java/com/tienda/controller/PersonaController.java
+++ src/main/java/com/tienda/controller/PersonaController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PersonaController {
@@ -37,7 +38,7 @@ public class PersonaController {
         return "crear";
     }
     
-    @GetMapping("/save")
+    @PostMapping("/save")
     public String guardarPersona (@ModelAttribute Persona persona){
         personaService.savePersona(persona);
         return "redirect:/persona";
--- src/main/java/com/tienda/entity/Pais.java
+++ src/main/java/com/tienda/entity/Pais.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="personas")
+@Table(name="paises")
 public class Pais implements Serializable{
     
     @Id
